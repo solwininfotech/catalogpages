7dfb74f0d56b4347396da143bd2240c1c6488508
Mayur Dhudasia <mmdhudasia@gmail.com>
2020-05-20 11:15:39 +0530

    Initial release

---
+++ registration.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Solwin Infotech
+ * Solwin 7 in 1 Catalog Manager Extension
+ *
+ * @category   Solwin
+ * @package    Solwin_CatalogPages
+ * @copyright  Copyright © 2006-2016 Solwin (https://www.solwininfotech.com)
+ * @license    https://www.solwininfotech.com/magento-extension-license/
+ */
+?>
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Solwin_CatalogPages',
+    __DIR__
+);
